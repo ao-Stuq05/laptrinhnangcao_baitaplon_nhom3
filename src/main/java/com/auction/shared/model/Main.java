ccb381354b227b975db136f2198c0dc3dd6702ec
Ao <235355126+ao-Stuq05@users.noreply.github.com>
2026-04-06 21:38:58 +0700

    Add files via upload

---
+++ src/main/java/com/auction/shared/model/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Seller extends User {
+  private String shopName;
+  private List<Item> listedItems;
+  private double rating;
+
+  public Seller(String id, String username, String email, String pw, String shopName) {
+    super(id, username, email, pw);
+    this.shopName = shopName;
+    this.listedItems = new ArrayList<>();
+  }
+
+  @Override
+  public String getRole() {
+    return "Seller";
+  }
+
+  public void listItem(Item item) {
+    listedItems.add(item);
+  }
+
+  public void createAuction(Item item) {
+    Auction auction = new Auction("AUC-" + System.currentTimeMillis(), item);
+    AuctionManager.getInstance().registerAuction(auction);
+  }
+}
