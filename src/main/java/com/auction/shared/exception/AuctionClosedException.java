0f94f9bc552fad51aa74c7f4fbd9faf5a8e942f0
Ao <235355126+ao-Stuq05@users.noreply.github.com>
2026-05-11 20:47:30 +0700

    sửa lỗi logic code

--- src/main/java/com/auction/shared/exception/AuctionClosedException.java
+++ src/main/java/com/auction/shared/exception/AuctionClosedException.java
@@ -6,15 +6,15 @@ public class AuctionClosedException extends Exception {
     private final AuctionStatus actualStatus; // trạng thái phiên lúc lỗi xảy ra
     private final String auctionId; // phiên xảy ra lỗi
 
-    public AuctionClosedException (AuctionStatus actualStatus,String auctionId) {
+    public AuctionClosedException (String auctionId, AuctionStatus actualStatus) {
         super(String.format(
                 "Phiên %s không thể đặt giá. Trạng thái hiện tại: %s",
                 auctionId, actualStatus));
-        this.auctionId = = auctionId;
+        this.auctionId = auctionId;
         this.actualStatus = actualStatus;
     }
 
-    public AuctionClosedException (String massage) {
+    public AuctionClosedException (String message) {
         super(message);
         this.auctionId    = null;
         this.actualStatus = null;
